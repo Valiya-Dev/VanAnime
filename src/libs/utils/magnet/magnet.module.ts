ccf7100eec07f7d9e55eead4b84c9f830be4160a
Lord Van <ziningwa@usc.edu>
2025-03-16 03:46:14 -0700

    修改eslint

--- src/libs/utils/magnet/magnet.module.ts
+++ src/libs/utils/magnet/magnet.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { TorrentTransformerService } from './torrent-transformer/torrent-transformer.service';
 
 export const importPackage = (packageName: string) =>
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-return
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-implied-eval,@typescript-eslint/no-unsafe-call
   new Function(`return import('${packageName}')`)().then(
     (loadedModule: { [x: string]: any }) =>
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       loadedModule['default'] ?? loadedModule,
   );
 
@@ -13,7 +12,6 @@ export const importPackage = (packageName: string) =>
   providers: [
     {
       provide: 'package:webtorrent',
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       useFactory: () => importPackage('webtorrent'),
     },
     TorrentTransformerService,
